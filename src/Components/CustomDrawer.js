7c046ba42fee09cc9f9a1d6090da8be85a527f96
Luma8 <lucasrobbot@gmail.com>
2021-12-10 10:17:06 -0300

    Changes-Drawer

---
+++ src/Components/CustomDrawer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+import planet from '../../assets/Icons/Planet.png';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from '../../assets/Icons/Quitbranco.png'
+
+const CustomDrawer = (props) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <DrawerContentScrollView style={estilos.buttons} {...props}>
+                <Image source={planet} style={estilos.planetS} />
+                <DrawerItemList  {...props} />
+            </DrawerContentScrollView>
+            <View style={{ padding: 20,}}>
+                <TouchableOpacity style={estilos.buttonStyles}>
+                    <Image source={Icon} style={estilos.IconQuit} />
+                    <Text style={estilos.textStyle}>Sair</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+export default CustomDrawer;
+
+const estilos = StyleSheet.create({
+    buttons: {
+        marginHorizontal: 10,
+        marginTop: 30,
+    },
+    planetS: {
+        height: 200,
+        width: 200,
+        top: -70,
+        right: -120,
+
+    },
+    buttonStyles: {
+        backgroundColor: '#577590',
+        borderRadius: 5,
+        width: 240,
+        height: 45,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    IconQuit: {
+        width: 30,
+        height: 25,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    textStyle: {
+        color: '#FFFFFF',
+        fontSize: 17,
+    }
+});
+
--- src/Routes/RotasTemporariasDw/DrawerRoutesInst.js
+++ src/Routes/RotasTemporariasDw/DrawerRoutesInst.js
@@ -1,97 +1,37 @@
 import React from "react";
-import { Text, StyleSheet, View, Image } from 'react-native';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+
+import CustomDrawer from "../../Components/CustomDrawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
 import Trilhas from '../../pages/DrawerStack/HomePages/Trilhas';
 import Sobre from '../../pages/DrawerStack/HomePages/Sobre';
 import Quiz from "../../pages/DrawerStack/HomePages/Quiz";
 import Instituicao from "../../pages/DrawerStack/HomePages/Instituicao";
-import planet from '../../../assets/Icons/Planet.png';
-import Icon from '../../../assets/Icons/Quitbranco.png';
-import ButtonSobre from "../../Components/ButtonsDrawer/ButtonSobre";
-import { DrawerActions } from "@react-navigation/native";
-
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Drawer = createDrawerNavigator();
 
-
-const CustomDrawer = (props, focused) => {
-    return (
-        <DrawerContentScrollView {...props}>
-            <View style={estilos.container}>
-                <Image source={planet} style={estilos.ImageStyle} />
-            </View>
-            <DrawerItemList {...props} />
-            <View style={estilos.containerButton}>
-                <TouchableOpacity style={estilos.buttonStyles}
-                    onPress={()=> props.navigation.dispatch(DrawerActions.closeDrawer)}  
-                >
-                    <Image source={Icon} style={estilos.IconQuit} />
-                    <Text style={estilos.textStyle}>Sair</Text>
-                </TouchableOpacity>
-            </View>
-        </DrawerContentScrollView>
-    );
-}
-
-export default function DrawerRoutes() {
+const DrawerRoutesInst = () => {
     return (
         <Drawer.Navigator
             screenOptions={{
                 headerShown: false,
+                drawerPosition: "right",
                 drawerStyle: {
                     backgroundColor: '#04252f',
                 },
-                drawerPosition: "right",
+                drawerActiveBackgroundColor: '#90BE6D',
+                drawerInactiveBackgroundColor: '#577590',
+                drawerActiveTintColor: '#fff',
+                drawerInactiveTintColor: '#fff',
             }}
+            drawerContent={props => <CustomDrawer {...props} />}
             initialRouteName="Instituição"
-            drawerContent={(props) => <CustomDrawer {...props} />}
         >
-            <Drawer.Screen name="Trilhas" component={Trilhas}
-                options={{
-                 
-                }}
-
-            />
-            <Drawer.Screen name="Sobre" component={Sobre}
-            />
-            <Drawer.Screen name="Quiz" component={Quiz}
-            />
-            <Drawer.Screen name="Instituição" component={Instituicao}
-            />
+            <Drawer.Screen name="Trilhas" component={Trilhas} />
+            <Drawer.Screen name="Sobre" component={Sobre} />
+            <Drawer.Screen name="Quiz" component={Quiz} />
+            <Drawer.Screen name="Instituição" component={Instituicao} />
         </Drawer.Navigator>
     );
 }
-
-const estilos = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    containerButton: {
-        alignItems: 'center',
-    },
-    buttonStyles: {
-        backgroundColor: '#577590',
-        borderRadius: 10,
-        width: 230,
-        height: 55,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    ImageStyle: {
-        height: 200,
-        width: 200,
-        top: -50,
-        right: -130,
-    },
-    IconQuit: {
-        width: 30,
-        height: 25,
-        marginLeft: 20,
-        marginRight: 20,
-    },
-    textStyle: {
-        color: '#FFFFFF',
-        fontSize: 17,
-    }
-});
+export default DrawerRoutesInst;
